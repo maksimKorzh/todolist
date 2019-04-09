857414663cf20ba6eb8931ccd63196d67bc62e48
Maksim Korzh <freesoft.for.people@gmail.com>
2019-04-09 13:45:45 +0300

    added part_8 code

---
+++ tutorial/part_8/api/task.js
@@ -0,0 +1,45 @@
+// Import mongoose
+const mongoose = require('mongoose');
+
+// Remove annoying deprication warning
+mongoose.set('useFindAndModify', false);
+
+// Define task schema
+const taskSchema = mongoose.Schema({
+  task:{
+    type: String,
+    require: true
+  },
+  create_date:{
+    type: Date,
+    default: Date.now
+  }
+});
+
+// Export task object
+const Task = module.exports = mongoose.model('Task', taskSchema);
+
+// Get task
+module.exports.getTask = (callback, limit) => {
+  Task.find(callback).limit(limit);
+}
+
+// Update task
+module.exports.updateTask = (id, updatedTask, options, callback) => {
+  var query = {_id: id};
+  var update = {
+    task: updatedTask.task
+  }
+  Task.findOneAndUpdate(query, update, options, callback);
+}
+
+// Add task
+module.exports.addTask = (task, callback) => {
+  Task.create(task, callback);
+}
+
+// Delete task
+module.exports.removeTask = (id, callback) => {
+  var query = {_id: id};
+  Task.deleteOne(query, callback);
+}
---
+++ tutorial/part_8/client/todo.js
@@ -0,0 +1,47 @@
+var app = angular.module('app');
+
+app.controller('TasksController', ['$scope', '$http', '$location', '$routeParams',
+                function($scope, $http, $location, $routeParams){
+  $scope.getTasks = function(){
+    $http.get('/api/tasks').then(function(response){
+      $scope.tasks = response;
+    }, function(error){
+      if(error){
+        throw error;
+      }
+    });
+  }
+
+  $scope.addTask = function() {
+    console.log($scope.task);
+    $http.post('/api/tasks', $scope.task).then(function(response){
+      window.location.href = "#";
+    }, function(error){
+      if(error){
+        throw error;
+      }
+    });
+  }
+
+  $scope.removeTask = function(id) {
+    console.log("removing task " + id);
+    $http.delete('/api/tasks/' + id).then(function(response){
+      window.location.href = "#";      
+    }, function(error){
+          if(error){
+            throw error;
+          }
+    });
+  }
+
+  $scope.updateTask = function(id){
+    		$http.put('/api/tasks/'+id, $scope.task).then(function(response){
+    			window.location.href='#';
+    		}, function(error) {
+    		  if(error){
+    		    throw error;
+    		  }
+    		});
+    }
+                   
+}]);
